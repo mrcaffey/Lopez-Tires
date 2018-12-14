58f050fd5fd3c6407357f3293db0ff4c430ea0e3
Micah Caffey <mrcaffey@gmail.com>
2018-12-14 13:42:03 -0700

    main logo image size is correct

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,10 @@ const ListLink = props => (
 export default ({ children }) => (
 <div>
 <div style={{ margin: `0 auto`, maxWidth: 960, padding: `0px 1.0875rem 1.45rem`, paddingTop: 0, }}>
-      <banner><img src="/images/LopezTires_newLogo.png"></img></banner>
+      <banner>
+        <img src="/images/LopezTires_newLogo.png"></img>
+        <img src="/images/LopezTires_tireTracks.png"></img>
+      </banner>
       <header style={{ marginBottom: `1.5rem`, marginTop: `1.5rem`}}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}></h3>
