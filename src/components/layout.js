87be00ca4747b3cace77b40b4f8468030e0d4f4d
Micah Caffey <mrcaffey@gmail.com>
2018-12-14 20:05:19 -0700

    tire track image shows..formatting needs serious work

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,10 +12,13 @@ const ListLink = props => (
 export default ({ children }) => (
 <div>
 <div style={{ margin: `0 auto`, maxWidth: 960, padding: `0px 1.0875rem 1.45rem`, paddingTop: 0, }}>
+      <div>
       <banner>
         <img src="/images/LopezTires_newLogo.png"></img>
-        <img src="/images/LopezTires_tireTracks.png"></img>
+        <img src="/images/LopezTires_tireTracks.png" style={{ flex:`1`, float: `right`, objectFit: `contain` }}></img>
+        <p>The banner is here</p>
       </banner>
+      </div>
       <header style={{ marginBottom: `1.5rem`, marginTop: `1.5rem`}}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}></h3>
